95d4dbec513bdbbcf53d1b999badb6112c035bfc
xiongjian <webjsforyou@163.com>
2019-06-21 17:05:25 +0800

    完成表单校验

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -13,6 +13,49 @@ class Login extends Component {
 
   login = (e) => {
     e.preventDefault();
+    // 用来校验表单并获取表单的值
+    this.props.form.validateFields((error, values) => {
+      // console.log(error, values);
+      /*
+        error 代表表单校验结果
+          null 校验通过
+          {} 校验失败
+       */
+      if (!error) {
+        // 校验通过
+        const { username, password } = values;
+        // 发送请求，请求登录
+        console.log(username, password);
+      } else {
+        // 校验失败
+        console.log('登录表单校验失败：', error);
+      }
+    })
+  }
+
+  /**
+   * 自定义校验规则函数
+   * @returns {*}
+   */
+  validator = (rule, value, callback) => {
+    // callback必须调用
+    // console.log(rule, value);
+
+    const name = rule.fullField === 'username' ? '用户名' : '密码';
+
+    if (!value) {
+      // 没有输入
+      callback(`必须输入${name}！`);
+    } else if (value.length < 4) {
+      callback(`${name}必须大于4位`);
+    } else if (value.length > 15) {
+      callback(`${name}必须小于15位`);
+    } else if (!/^[a-zA-Z_0-9]+$/.test(value)) {
+      callback(`${name}只能包含英文字母、数字和下划线`);
+    } else {
+      // 不传参代表校验通过，传参代表校验失败
+      callback();
+    }
 
   }
 
@@ -34,10 +77,13 @@ class Login extends Component {
                 'username',
                 {
                   rules: [
-                    {required: true, message: '请输入用户名！'},
+                    /*{required: true, message: '请输入用户名！'},
                     {min: 4, message: '用户名必须大于4位'},
                     {max: 15, message: '用户名必须小于15位'},
-                    {pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'}
+                    {pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'}*/
+                    {
+                      validator: this.validator
+                    }
                   ]
                 }
               )(
@@ -46,7 +92,20 @@ class Login extends Component {
             }
           </Item>
           <Item>
-            <Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>
+            {
+              getFieldDecorator(
+                'password',
+                {
+                  rules: [
+                    {
+                      validator: this.validator
+                    }
+                  ]
+                }
+              )(
+                <Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>
+              )
+            }
           </Item>
           <Item>
             <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
@@ -57,6 +116,6 @@ class Login extends Component {
   }
 }
 
-// 返回值是一个包装组件   <Form(Login)><Login><Form(Login)>
+// 返回值是一个包装组件   <Form(Login)><Login></Form(Login)>
 // 通过Form(Login)包装组件向Login组件中传递form属性
 export default Form.create()(Login);
